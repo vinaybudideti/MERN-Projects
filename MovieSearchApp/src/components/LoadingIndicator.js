04a1ad7ec909efa2210d5004c6a95aad69a9b89c
Vinay Kumar Reddy Budideti <vinaykumarreddy.budideti@gmail.com>
2025-04-01 13:06:49 -0500

    Initial commit for project3

--- MovieSearchApp/src/components/LoadingIndicator.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LoadingIndicator = () => {
-  return <div className="text-center">Loading...</div>;
-};
-
-export default LoadingIndicator;
--- MovieSearchApp/src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import movieReducer from './reducers/movieReducer';
-
-const store = createStore(movieReducer);
-
-export default store;
